c06b24ebfe7c5fec083f0b9d89e4ec16033671f2
Jorge Capona <jcapona@gmail.com>
2016-02-18 14:25:15 -0300

    Send messages on authentication

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -2,35 +2,42 @@ var passport = require('passport');
 
 module.exports = {
 
-    _config: {
-        actions: false,
-        shortcuts: false,
-        rest: false
-    },
+  _config: {
+    actions: false,
+    shortcuts: false,
+    rest: false
+  },
 
-    login: function(req, res) {
+  login: function(req, res) {
 
-        passport.authenticate('local', function(err, user, info) {
-            if ((err) || (!user)) {
-                return res.send({
-                    message: info.message,
-                    user: user
-                });
-            }
-            req.logIn(user, function(err) {
-                if (err) res.send(err);
-                return res.send({
-                    message: info.message,
-                    user: user
-                });
-            });
+    passport.authenticate('local', function(err, user, info) {
+      if ((err) || (!user)) {
+        req.session.flash = {
+          error: info.message
+        }
+        return res.redirect('/signin');
+        // return res.send({ message: info.message, user: user });
+      }
+      req.logIn(user, function(err) {
+        if (err) 
+          res.send(err);
+         
+        req.session.flash = {
+          success: info.message
+        } 
+        return res.redirect('/');
+        //return res.send({ message: info.message, user: user });
+      });
 
-        })(req, res);
-    },
+    })(req, res);
+  },
 
-    logout: function(req, res) {
-        req.logout();
-        res.redirect('/');
-    }
+  logout: function(req, res) {
+    req.logout();
+    req.session.flash = {
+      success: "Succesfully logged out. Come back later!"
+    } 
+    res.redirect('/');
+  }
 };
 
