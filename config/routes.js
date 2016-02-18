44944ed2660db351dd8687aa3696b98bcc29d6ae
Jorge Capona <jcapona@gmail.com>
2016-02-18 12:46:13 -0300

    New routes for authentication

--- config/routes.js
+++ config/routes.js
@@ -22,36 +22,13 @@
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
-
   '/': {
     view: 'index'
   },
   '/signin': {
     view: 'signin'
   },
-  'user': {
-    'new': true,
-    index: 'sessionAuth',
-    '*': true
-  }
-
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
+  'post /login': 'AuthController.login',
+  '/logout': 'AuthController.logout', 
 
 };
