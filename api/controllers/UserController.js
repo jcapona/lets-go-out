ba927f3f66b9641a4a603c39ea50b14fd9dbdb47
Jorge Capona <jcapona@gmail.com>
2016-02-24 20:36:53 -0300

    Added register controller to create new users in db

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -12,6 +12,27 @@ module.exports = {
 			return res.send({user: req.user});
 		else
 			return res.send({});
+	},
+	register: function(req, res) {
+	  User.create(req.params.all()).exec(function (err, user) {
+	    if (err)
+      {
+        req.session.flash = {
+          error: "Error registering: Username/Email already taken/used"
+        } 
+	    	return res.redirect("/index");
+      }
+
+	    req.login(user, function (err){
+	      if (err) 
+        {
+          req.session.flash = {
+           error: "Error logging in. Please try again."
+          } 
+        }
+	      return res.redirect('/index');
+	    });
+	  });
 	}
-	
+
 };
