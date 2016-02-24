eb0baa71fd2d0e325a6a218c58594b57ef66795f
Jorge Capona <jcapona@gmail.com>
2016-02-24 15:34:55 -0300

    Added mongodb mongolab server

--- config/env/development.js
+++ config/env/development.js
@@ -16,9 +16,18 @@ module.exports = {
    * Set the default database connection for models in the development       *
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
-
-  // models: {
-  //   connection: 'someMongodbServer'
-  // }
-
+   
+   connections: {
+	    mongolabServer: {
+		    adapter: 'sails-mongo',
+		    host: 'ds011268.mongolab.com',
+		    port: 11268,
+		    user: 'example',
+		    password: 'example',
+		    database: 'letsgoout'
+		}
+	},
+   	models: {
+		connection: 'mongolabServer'
+  	},
 };
