92c199c3d918ec96255857a98559c64e7be431bb
Jorge Capona <jcapona@gmail.com>
2016-02-25 10:28:09 -0300

    Added Attendance model, used to count people going to events

---
+++ api/models/Attendance.js
@@ -0,0 +1,21 @@
+/**
+ * Attendance.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+  		user_id: {
+            type: 'string',
+        },
+        event_id: {
+        	type: 'string',
+        },
+        toJSON: function() {
+            return this.toObject();
+        }
+  }
+};
